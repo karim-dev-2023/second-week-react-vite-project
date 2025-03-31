2a210dfa692e3a246f980760a84529d4c7ffba8a
Karim <karim.ayoub.job@gmail.com>
2025-03-31 13:35:23 +0100

    Correction de l'activité 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,42 @@
 import "./App.css";
 
 import { useEffect, useState } from "react";
-import { Container, Row, Button, Card } from "react-bootstrap/";
+import { Container, Row, Button, Card, Col } from "react-bootstrap/";
 
 function App() {
-  const [products, setProduct] = useState(null);
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
     async function fetchProduct() {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-      setProduct(data);
+      setProducts(data);
     }
     fetchProduct();
   }, []);
 
   return (
-    <Container >
-      <Row className="rowClass">
+    <Container fluid>
+      <Row>
         {products &&
           products.map((products) => (
-            <Card key={products.id}>
-              <Card.Img
-                variant="top"
-                src={products.image}
-                alt={products.title}
-                fluid
-              />
-              <Card.Body>
-                <Card.Title>{products.title}</Card.Title>
-                <Card.Text className="text-truncate ">{products.description}</Card.Text>
-                <Button variant="primary">{products.price} $</Button>
-              </Card.Body>
-            </Card>
+            <Col className="m-3">
+              <Card key={products.id}>
+                <Card.Img
+                  variant="top"
+                  src={products.image}
+                  alt={products.title}
+                  fluid
+                />
+                <Card.Body>
+                  <Card.Title>{products.title}</Card.Title>
+                  <Card.Text className="text-truncate ">
+                    {products.description}
+                  </Card.Text>
+                  <Button variant="primary">{products.price} $</Button>
+                </Card.Body>
+              </Card>
+            </Col>
           ))}
       </Row>
     </Container>
