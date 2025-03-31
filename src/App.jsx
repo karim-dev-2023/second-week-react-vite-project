98182cdade9053c370251642ec43f0c7ed8215cf
Karim <karim.ayoub.job@gmail.com>
2025-03-31 16:58:17 +0200

    Activité 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { useEffect, useState } from "react";
-import { Container, Row, Button, Card, Col } from "react-bootstrap/";
+import { Container, Row, Button, Card, Col, Stack } from "react-bootstrap/";
 
 function App() {
   const [products, setProducts] = useState(null);
@@ -15,8 +15,32 @@ function App() {
     fetchProduct();
   }, []);
 
+  async function addProduct() {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST", // Indique qu'on envoie des données pour créer une nouvelle ressource
+      headers: {
+        "Content-Type": "application/json", // Indique que les données envoyées sont au format JSON
+      },
+      body: JSON.stringify({
+        // Convertit l'objet JavaScript en une chaîne JSON
+        title: "Nouveau produit",
+        price: 29.99,
+        description: "Un super produit ajouté via API",
+        image: "https://via.placeholder.com/150",
+        category: "electronics",
+      }),
+    });
+    const data = await response.json();
+    alert(`Le produit avec l'id ${data.id} a été créé`);
+  }
+
   return (
     <Container fluid className="w-75">
+      <Stack className="mt-5">
+        <Button variant="primary" onClick={addProduct}>
+          Ajouter un produit
+        </Button>
+      </Stack>
       <Row className="mt-5">
         {products &&
           products.map((products) => (
