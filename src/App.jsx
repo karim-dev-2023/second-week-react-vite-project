9f48a2cd2c20fcffe5fea79c64a5967d3e37830a
Karim <karim.ayoub.job@gmail.com>
2025-04-01 08:52:38 +0200

    Activité 7

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,59 @@ function App() {
     const data = await response.json();
     alert(`Le produit avec l'id ${data.id} a été créé`);
   }
+  async function updateProduct(productId) {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/${productId}`,
+      {
+        method: "PUT", // Indique qu'on envoie des données pour créer une nouvelle ressource
+        headers: {
+          "Content-Type": "application/json", // Indique que les données envoyées sont au format JSON
+        },
+        body: JSON.stringify({
+          // Convertit l'objet JavaScript en une chaîne JSON
+          title: "Nouveau produit",
+          price: 29.99,
+          description: "Un super produit ajouté via API",
+          image: "https://via.placeholder.com/150",
+          category: "electronics",
+        }),
+      }
+    );
+    const data = await response.json();
+
+    alert(`Le produit avec l'id ${data.id} a été mis à jour`);
+  }
+
+  async function updatePriceProduct(productId) {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/${productId}`,
+      {
+        method: "PATCH", // Indique qu'on envoie des données pour créer une nouvelle ressource
+        headers: {
+          "Content-Type": "application/json", // Indique que les données envoyées sont au format JSON
+        },
+        body: JSON.stringify({
+          // Convertit l'objet JavaScript en une chaîne JSON
+          price: 5,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    alert(`Le prix avec l'id ${data.id} a été mis à jour`);
+  }
+  async function deleteProduct(productId) {
+    const response = await fetch(
+      `https://fakestoreapi.com/products/${productId}`,
+      {
+        method: "DELETE", // Indique qu'on supprime une ressource existante
+      }
+    );
+    const data = await response.json();
+
+    alert(`Le produit avec l'id ${data.id} a été supprimé`);
+  }
 
   return (
     <Container fluid className="w-75">
@@ -56,6 +109,24 @@ function App() {
                   <Card.Title>{products.title}</Card.Title>
                   <Card.Text>{products.description}</Card.Text>
                   <Card.Text variant="primary">{products.price} $</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => updateProduct(products.id)}
+                  >
+                    Modifier le produit complet{" "}
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={() => updatePriceProduct(products.id)}
+                  >
+                    Modifier le prix{" "}
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => deleteProduct(products.id)}
+                  >
+                    Supprimer le produit
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
